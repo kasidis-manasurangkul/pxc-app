4e95c0228ec89b8c8c1cd2047af8efb25b8da948
kasidis-manasurangkul <84066800+kasidis-manasurangkul@users.noreply.github.com>
2025-03-26 19:55:05 +0700

    add sign in page for both admin and user

--- pcx-user-app/src/app/app-routing.module.ts
+++ pcx-user-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { RecommendationComponent } from './pages/recommendation/recommendation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'recommendation', pathMatch: 'full' },
+    { path: 'sign-in', component: SignInComponent },
+    { path: 'recommendation', component: RecommendationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pcx-user-app/src/app/app.module.ts
+++ pcx-user-app/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { RecommendationComponent } from './pages/recommendation/recommendation.component';
+import { LibraryComponent } from './pages/library/library.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    RecommendationComponent,
+    LibraryComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+      BrowserModule,
+      AppRoutingModule,
+      FormsModule,
+      RouterModule,
+      CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
